4df313d7661487f19a2609238aa6843ee88eb6ce
rajvirsingh <rajvirsinghg617@gmail.com>
2024-08-21 16:45:18 +0530

    NEW

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cart;
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        $cartDatas=[
+            'cartitem1'=>['product name'=>'Ultra Wireless S50 Headphones S50 with Bluetooth',
+            'unit_price'=>'100',
+            'qty'=>2,
+            'total'=>200,
+            
+        ],
+            'cartitem2'=>['product name'=>'Ultra Wireless S50',
+            'unit_price'=>'1000',
+            'qty'=>5,
+            'total'=>5000,
+            
+        ]
+        ];
+        $grandTotal=5200;
+        return view("shop/cart",['cartDatas' => $cartDatas,
+                                'grandTotal'=>$grandTotal]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Cart $cart)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Cart $cart)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Cart $cart)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Cart $cart)
+    {
+        //
+    }
+}
